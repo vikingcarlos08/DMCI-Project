237e4aed9c75876c916e9cb74d2d03fc3dae368f
Juan Carlos Rioja <Juan@FURTHEREDGE.COM>
2020-02-28 17:48:53 +0800

    Adding Bootstrap Template

--- DMCI-client/src/app/app.module.ts
+++ DMCI-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
 
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
